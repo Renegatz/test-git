5c4e8f804a83045d14ce4c98ac7bcaaca330dbda
git <git@LD140-12-06285.local>
2017-10-16 09:33:07 -0400

    Je suis aussi la ligne 15

--- test-git/ViewController.swift
+++ test-git/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("je suis aussi la ligne 15: la revenge du dernier print")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
